42a3d2d4b8f803df67f26a2d1e0c0df1322dd8c1
luciancalian <luciancalian@gmail.com>
2021-03-01 12:24:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/fargate", (req, res) => {
+app.get("/new-fargate", (req, res) => {
   res.send("Hello Automated build!");
 });
 
