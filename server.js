eaef51601992cd4162fde77266d88105d7a73691
luciancalian <luciancalian@gmail.com>
2021-03-09 18:07:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,25 +3,8 @@ const httpRequest = require("https");
 const app = express();
 const port = 3000;
 
-const options = {
-  hostname: 'reqres.in',
-  port: 443,
-  path: '/api/users',
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-}
-
-app.get("/new-fargate", (req, res) => {
-  const newreq = httpRequest.request(options, res1 => {
-    console.log(`statusCode: ${res1.statusCode}`)
-    res1.on('data', d => {
-      process.stdout.write(d);
-      console.log(d);
-    });
-  });
-  res.send("Hello Automated build!");
+app.get("/ec2", (req, res) => {
+  res.send("Hello EC2 build!");
 });
 
 app.get("/new-route", (req, res) => {
